00e1cccf3c064652fd5ed6a350f285433be72f40
pavan <sun237539@gmail.com>
2017-12-31 11:19:18 +0530

    committed Successfully

---
+++ onlineshopping/src/main/java/org/pav/onlineshopping/Controller/PageController.java
@@ -0,0 +1,18 @@
+package org.pav.onlineshopping.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value="/hello")
+	public ModelAndView Hello(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", "Hello Spring MVC!!");
+		return mv;
+		
+	}
+	
+}
